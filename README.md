090149bd3a137e32d2fb8ea26bde4cc71dc7bea7
cutpatel <cutpatel@fz-borstel.de>
2019-08-08 16:02:48 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,111 @@
+<img src="https://github.com/ngs-fzb/binoSNP/blob/master/images/binoSNP_logo.png" height="153" width="400">
+
+
+*****
+
+SNP detection based on binomial test procedure. Perl scripts with R integration and usage of bam-readcount algorithm. 
+
+<p align="center">
+ <img src="https://github.com/ngs-fzb/binoSNP/blob/master/images/workflow.png" height="401" width="300">
+</p>
+
+*****
+ 
+The script accepts any BAM-file as input, but ideally duplicates (PCR artefacts) have been removed and base quality scores have been recalibrated. Additionally, the script requires an interval list where the positions to be examined are named as well as a RefAlt table defining reference and the alternative allele for those positions. Together with BinoSNP we provide two example lists specifically for the analysis of TB bacteria. The first list (PhyloSNPs) includes lineage-specific SNP positions and the second list (ResiSNPs) includes positions known to be associated with drug resistance. As a first step the bam-readcount algorithm from Larson [bam-readcount](https://github.com/genome/bam-readcount) is executed to extract information about the number and quality of reference and alternative alleles at the positions named in the interval list and stores this information in a text file. In a second step the resulting txt-file is read into R and for each position a p-value is calculated by using the binomial test procedure. In the next step a table is produced containing all information including the calculated p-value for each position named in the interval list. The last step implies the user-defined filtering, e.g. report variants with a p-value below 5 % (standard value for statistical significance).
+
+## Getting Started
+To run the script the user needs a running perl 
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+As binoSNP relies on R to work, you need to install R first. See this page for downloads, [R project page](http://www.r-project.org/). 
+Additionally, you need to install the perl module "Statistics::R". You can install this module using the following command:
+
+```
+cpan Statistics::R
+```
+Another prerequisite is the bam-readcount algorithm. See this page to download and install, [bam-readcount](https://github.com/genome/bam-readcount).
+### Installing
+The script do not need to be installed. Once downloaded you can run it from the console.
+
+
+## Usage
+Use
+```
+binoSNP --help
+```
+will print the following help message:
+```
+binoSNP 1.0.0 - Copyright (C) 2019  Viola Dreyer, Christian Utpatel
+   
+   [USAGE]: binoSNP [--OPTION PARAMETER] <.bam file>
+   
+   Available OPTIONS and default PARAMETERS:
+   -i [--interval]      List of intervals to be analyzed
+                        Default [Resisnps_Master.v28.interval_list.tsv]
+
+   -m [--mut]           Mutation table (RefAlt table)
+                        Default [Resisnps_Master.v28_RefuAlt.tsv]
+
+   -o [--outdir]        Output directory
+                        Default [./Low_Freq]
+
+   -r [--ref]           Reference sequence used for aligment in fasta format
+                        Default [M._tuberculosis_H37Rv_2015-11-13.fasta]
+
+   -p [--pvalue]        p-value used to filter the results
+                        Default [0.05]
+
+   -h [--help]          This help message
+
+   -v [--version]       Version of binoSNP
+   ```
+You can either run a single file by using:
+```
+binoSNP /path/to/your/filename.bam
+```
+Or you run all bamfiles of your directory as a batch:
+```
+binoSNP path/to/your/bamfiles/*.bam
+```
+
+
+
+In case you need other positions analyzed, you can create and use your own intervals. The list of intervals should follow the structure below:
+```
+M.tuberculosis_H37Rv \t start1 \t stop1 \n
+M.tuberculosis_H37Rv \t start2 \t stop2 \n
+M.tuberculosis_H37Rv \t start3 \t stop3 \n
+M.tuberculosis_H37Rv \t start4 \t stop4 \n
+```
+For annotation of the positions, the user should also provide a mutation list in the following format:
+```
+Pos \t REF \t ALT \t Annotation \n
+761140 \t A \t G \t RMP resistance \n
+```
+Usage of an own list:
+```
+binoSNP -i path/to/your/intervavl.list -m path/to/your/mutations.list
+```
+
+
+
+## Authors
+
+* **Viola Dreyer** - *Initial work* - [binoSNP](https://github.com/ngs-fzb/binoSNP)
+* **Christian Utpatel** - *Code contribution* 
+* Stefan Niemann - *Head*
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+Copyright (C) 2019 Viola Dreyer, Christian Utpatel, Stefan Niemann. This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see [LICENCES](https://www.gnu.org/licenses/gpl-3.0).
+
+##Publication
+This work is published. Please cite:
+[citation]()
+
+## Acknowledgments
+
+Parts of this work have been supported by the German Center for Infection Research (DZIF).
+
---
+++ bin/readcount2table_binomial.pl
@@ -0,0 +1,275 @@
+#!/usr/bin/perl
+use Statistics::R;
+use List::Util qw/sum/;
+
+$num_args = $#ARGV + 1;
+
+if ($num_args != 3) {
+   print "\n Verwendung: readcount2table.pl /Pfad/zur/Ausgabe/dateiname.table /Pfad/zur/Eingabe.txt /Pfad/zur/Snps_RefuAlt.table\n";
+    exit;
+}
+open(Fout,">$ARGV[0]") or die "\n\n\"Offnen von $ARGV[0] nicht m\"oglich\n\n\n";
+open(Fin,"<$ARGV[1]") or die "\n\n\"Offnen von $ARGV[1] nicht m\"oglich\n\n\n";
+open(Fin2,"<$ARGV[2]") or die "\n\n\"Offnen von $ARGV[2] nicht m\"oglich\n\n\n";
+
+@ref=<Fin2>;#Zeilen der RefuAlt Tabelle in einem Array Speichern
+print Fout "POS\tREF\tALT\tDP\t#A\tQual(A)\tFreq(A)\t#C\tQual(C)\tFreq(C)\t#G\tQual(G)\tFreq(G)\t#T\tQual(T)\tFreq(T)\tType\tFreq(ALT)\tQual(ALT)\tp-value\tAntibiotic\n";
+
+while($line=<Fin>){
+@array=split("\t", $line); #Jede Zeile des bam-readcount Datei nach Tabs auftrennen
+  unless(defined $h{$array[1]}){ #Doppeleintr\"age nur einmal verarbeiten
+@As=split(":", $array[5]); #Informationen zur Base A nach : auftrennen
+@Cs=split(":", $array[6]);#Informationen zur Base C nach : auftrennen
+@Gs=split(":", $array[7]);#Informationen zur Base G nach : auftrennen
+@Ts=split(":", $array[8]);#Informationen zur Base T nach : auftrennen
+@DEL=split(":", $array[10]);#Informationen ob es Deletion gibt nach : auftrennen
+@index= grep(/^$array[1]/i,@ref); #Position der bam-eadcount-Datei mit der Position der RefuAlt-Tabelle matchen
+
+
+$anzahl=@index; #Pr\"ufvariable, ob es mehrere alternative Basen und somit Doppeleintr\"age in der Resiliste gibt
+if($array[3] == 0){#Abfangen der illegal division by zero
+@zeile=split("\t", $index[0]);
+		  chomp($zeile[3]);
+print Fout "$zeile[0]\t$zeile[1]\t$zeile[2]\t$array[3]\t$As[1]\t$As[3]\t0\t$Cs[1]\t$Cs[3]\t0\t$Gs[1]\t$Gs[3]\t0\t$Ts[1]\t$Ts[3]\t-\t-\t-\t-\t-\t$zeile[3]\n";
+}
+elsif(defined $DEL[0]){#Abfrage, ob es sich um eine Deletion handelt
+    @zeile=split("\t", $index[0]);
+		  chomp($zeile[3]);
+    print Fout "$zeile[0]\t$zeile[1]\t$zeile[2]\t$array[3]\t$As[1]\t$As[3]\t0\t$Cs[1]\t$Cs[3]\t0\t$Gs[1]\t$Gs[3]\t0\t$Ts[1]\t$Ts[3]\t0\tDEL\t-\t-\t-\t$DEL[0] in $DEL[1] reads\n";}
+else{
+$freqA=$As[1]/$array[3];#Frequenz der Basen bestimmen
+$freqC=$Cs[1]/$array[3];
+$freqG=$Gs[1]/$array[3];
+$freqT=$Ts[1]/$array[3];
+
+my $R = Statistics::R->new();
+$R->startR ;
+#nun wird die Pr\"ufvariable abgefragt und je nach Anzahl der Eintr\"age in der Resiliste gibt es 1, 2 oder 3 Eintr\"age in der Tabelle
+if ($anzahl == 1) {@zeile=split("\t", $index[0]);
+		  chomp($zeile[3]);
+		  if ($zeile[2] eq "A"){$freqALT=$freqA;
+				      $qualALT=$As[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($As[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile[2] eq "C") {$freqALT=$freqC;
+				      $qualALT=$Cs[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Cs[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile[2] eq "G") {$freqALT=$freqG;
+				      $qualALT=$Gs[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Gs[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile[2] eq "T") {$freqALT=$freqT;
+				      $qualALT=$Ts[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Ts[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		  else {print "Fehler Else1\n";}
+	print Fout "$zeile[0]\t$zeile[1]\t$zeile[2]\t$array[3]\t$As[1]\t$As[3]\t$freqA\t$Cs[1]\t$Cs[3]\t$freqC\t$Gs[1]\t$Gs[3]\t$freqG\t$Ts[1]\t$Ts[3]\t$freqT\tSNP\t$freqALT\t$qualALT\t$Q\t$zeile[3]\n";
+		    $h{$array[1]}=1;
+}
+  elsif ($anzahl == 2) {
+		@zeile=split("\t", $index[0]);
+		@zeile2=split("\t", $index[1]);
+		chomp($zeile[3]);
+		  if ($zeile[2] eq "A"){$freqALT=$freqA;
+				      $qualALT=$As[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($As[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile[2] eq "C") {$freqALT=$freqC;
+				      $qualALT=$Cs[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Cs[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile[2] eq "G") {$freqALT=$freqG;
+				      $qualALT=$Gs[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Gs[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile[2] eq "T") {$freqALT=$freqT;
+				      $qualALT=$Ts[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Ts[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		  else {print "Fehler Else2\n";}
+		 chomp($zeile2[3]);
+		  if ($zeile2[2] eq "A"){$freqALT2=$freqA;
+				      $qualALT2=$As[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($As[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q2=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile2[2] eq "C") {$freqALT2=$freqC;
+				      $qualALT2=$Cs[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Cs[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q2=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile2[2] eq "G") {$freqALT2=$freqG;
+				      $qualALT2=$Gs[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Gs[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q2=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile2[2] eq "T") {$freqALT2=$freqT;
+				      $qualALT2=$Ts[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Ts[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q2=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		  else {print "Fehler Else3\n";}
+	print Fout "$zeile[0]\t$zeile[1]\t$zeile[2]\t$array[3]\t$As[1]\t$As[3]\t$freqA\t$Cs[1]\t$Cs[3]\t$freqC\t$Gs[1]\t$Gs[3]\t$freqG\t$Ts[1]\t$Ts[3]\t$freqT\tSNP\t$freqALT\t$qualALT\t$Q\t$zeile[3]\n";
+	print Fout "$zeile2[0]\t$zeile2[1]\t$zeile2[2]\t$array[3]\t$As[1]\t$As[3]\t$freqA\t$Cs[1]\t$Cs[3]\t$freqC\t$Gs[1]\t$Gs[3]\t$freqG\t$Ts[1]\t$Ts[3]\t$freqT\tSNP\t$freqALT2\t$qualALT2\t$Q2\t$zeile2[3]\n";
+	$h{$array[1]}=1;
+   }
+  elsif ($anzahl == 3) {
+		@zeile=split("\t", $index[0]);
+		@zeile2=split("\t", $index[1]);
+		@zeile3=split("\t", $index[2]);
+		chomp($zeile[3]);
+		  if ($zeile[2] eq "A"){$freqALT=$freqA;
+				      $qualALT=$As[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($As[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile[2] eq "C") {$freqALT=$freqC;
+				      $qualALT=$Cs[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Cs[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile[2] eq "G") {$freqALT=$freqG;
+				      $qualALT=$Gs[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Gs[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile[2] eq "T") {$freqALT=$freqT;
+				      $qualALT=$Ts[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Ts[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    else {print "Fehler Else4\n";}
+		chomp($zeile2[3]);
+		  if ($zeile2[2] eq "A"){$freqALT2=$freqA;
+				      $qualALT2=$As[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($As[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q2=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile2[2] eq "C") {$freqALT2=$freqC;
+				      $qualALT2=$Cs[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Cs[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q2=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile2[2] eq "G") {$freqALT2=$freqG;
+				      $qualALT2=$Gs[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Gs[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q2=$R->get('p');#mittlerer zu erwartender Sequenzierfehler
+				      }
+		    elsif ($zeile2[2] eq "T") {$freqALT2=$freqT;
+				      $qualALT2=$Ts[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Ts[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q2=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    else {print "Fehler Else5\n";}
+		 chomp($zeile3[3]);
+		  if ($zeile3[2] eq "A"){$freqALT3=$freqA;
+				      $qualALT3=$As[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($As[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q3=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile3[2] eq "C") {$freqALT3=$freqC;
+				      $qualALT3=$Cs[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Cs[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q3=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile3[2] eq "G") {$freqALT3=$freqG;
+				      $qualALT3=$Gs[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Gs[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q3=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		    elsif ($zeile3[2] eq "T") {$freqALT3=$freqT;
+				      $qualALT3=$Ts[3];
+				      $R->run(qq'quality<-c($As[3],$Cs[3],$Gs[3],$Ts[3])
+				      meanerror<-10^(-mean(quality[which(quality>0)])/10)
+				      p<-binom.test($Ts[1],$array[3],meanerror,alternative="greater", conf.level=0.95)');
+				      $R->run(q'p<-p$p.value');
+				      $Q3=$R->get('p');#mittlerer zu erwartender Fehler
+				      }
+		  else {print "Fehler Else6\n";}
+	print Fout "$zeile[0]\t$zeile[1]\t$zeile[2]\t$array[3]\t$As[1]\t$As[3]\t$freqA\t$Cs[1]\t$Cs[3]\t$freqC\t$Gs[1]\t$Gs[3]\t$freqG\t$Ts[1]\t$Ts[3]\t$freqT\tSNP\t$freqALT\t$qualALT\t$Q\t$zeile[3]\n";
+	print Fout "$zeile2[0]\t$zeile2[1]\t$zeile2[2]\t$array[3]\t$As[1]\t$As[3]\t$freqA\t$Cs[1]\t$Cs[3]\t$freqC\t$Gs[1]\t$Gs[3]\t$freqG\t$Ts[1]\t$Ts[3]\t$freqT\tSNP\t$freqALT2\t$qualALT2\t$Q2\t$zeile2[3]\n";
+	print Fout "$zeile3[0]\t$zeile3[1]\t$zeile3[2]\t$array[3]\t$As[1]\t$As[3]\t$freqA\t$Cs[1]\t$Cs[3]\t$freqC\t$Gs[1]\t$Gs[3]\t$freqG\t$Ts[1]\t$Ts[3]\t$freqT\tSNP\t$freqALT3\t$qualALT3\t$Q3\t$zeile3[3]\n";
+	$h{$array[1]}=1;
+  }
+else{print "Was ist mit Position $array[1]?\n"}  #Fehler abfangen
+
+ $R->stopR() ;} } #unless
+} #while
